899a40167623efcc50dad00fea429755e7220191
amandine.sahl <amandine.sahl@gmail.com>
2019-10-01 10:49:41 +0200

    Preparation installation

--- install.sh
+++ install.sh
@@ -1,8 +1,27 @@
 
-
 . settings.ini
 
+DIR=$(readlink -e "${0%/*}")
+echo "Arret de l'application..."
+sudo -s supervisorctl stop ${app_name}
+
+
+#Création des répertoires systèmes
+mkdir -p var/log
+
+# Backend
 cd backend
+
+echo "Création du fichier de configuration ..."
+if [ ! -f config.py ]; then
+  cp config.py.sample config.py
+fi
+
+
+echo "préparation du fichier config.py..."
+sed -i "s/SQLALCHEMY_DATABASE_URI = .*$/SQLALCHEMY_DATABASE_URI = \"postgresql:\/\/$user_pg:$user_pg_pass@$db_host:$db_port\/$db_name\"/" config.py
+
+
 rm -r venv
 virtualenv -p /usr/bin/python3 venv
 
@@ -12,3 +31,28 @@ pip install -r requirements.txt
 deactivate
 
 
+cd ..
+
+
+# Front end
+cd frontend/flora-occitania/
+#création d'un fichier de configuration
+if [ ! -f src/app/appSettings.ts ]; then
+  echo 'Fichier de configuration non existant'
+  cp src/app/appSettings.ts.sample src/app/appSettings.ts
+fi
+
+
+
+npm install
+
+ng build --prod --base-href /flora_occitania
+
+
+#Lancement de l'application
+cd ${DIR}
+sudo -s cp flora_occitania-service.conf /etc/supervisor/conf.d/
+sudo -s sed -i "s%APP_PATH%${DIR}%" /etc/supervisor/conf.d/flora_occitania-service.conf
+
+sudo -s supervisorctl reread
+sudo -s supervisorctl reload
