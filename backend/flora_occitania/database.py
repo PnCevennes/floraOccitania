8d29065dafd4c86753787d9521fd862e3bb8a71f
Amandine <amandine.sahl@cevennes-parcnational.fr>
2026-05-19 18:42:59 +0200

    Update backend dependencies, use systemd (#6)
    * Update backend dependencies, use systemd
    
    * Add service file

--- backend/flora_occitania/database.py
+++ backend/flora_occitania/database.py
@@ -1,17 +1,17 @@
-from os import environ
+import os
 from importlib import import_module
 
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
 
-db_path = environ.get("FLASK_SQLALCHEMY_DB")
-if db_path:
-    db_module_name, db_object_name = db_path.rsplit(".", 1)
-    db_module = import_module(db_module_name)
-    db = getattr(db_module, db_object_name)
-else:
-    db = SQLAlchemy()
-    environ["FLASK_SQLALCHEMY_DB"] = "flora_occitania.database.db"
-
-import os
+os.environ["FLASK_SQLALCHEMY_DB"] = "flora_occitania.database.db"
+DB = db = SQLAlchemy()
+# db_path = environ.get("FLASK_SQLALCHEMY_DB")
+# if db_path:
+#     db_module_name, db_object_name = db_path.rsplit(".", 1)
+#     db_module = import_module(db_module_name)
+#     db = getattr(db_module, db_object_name)
+# else:
+#     db = SQLAlchemy()
+#     environ["FLASK_SQLALCHEMY_DB"] = "flora_occitania.database.db"
